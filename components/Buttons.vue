b552af0c04075f69a4d4b5ae1d0e56ab7d4e2ed4
lusacramento <lucianossacramento@yahoo.com.br>
2024-01-22 16:31:28 -0300

    feat buttons group component

---
+++ components/Buttons.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="buttons">
+		<button type="button" @click.prevent="reset">Reset</button>
+		<button type="button" @click.prevent="copyToClipboard">Copy</button>
+	</div>
+</template>
+
+<style>
+	button {
+		background-color: #8d81f3;
+		color: #fff;
+		display: inline-block;
+		font-size: 20px;
+		padding: 10px;
+		outline: none;
+		border: none;
+		margin-right: 10px;
+	}
+</style>
